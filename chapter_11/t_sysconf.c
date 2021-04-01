4d4a9895a54b27a6322e09643beb64c4cb46bbd9
Wei jianxing <jx.wei@outlook.com>
2021-04-01 21:14:57 +0800

    update code

---
+++ chapter_11/t_sysconf.c
@@ -0,0 +1,36 @@
+#include "tlpi_hdr.h"
+
+static void sysconfPrint(const char* msg, int name)
+{
+    long lim;
+
+    errno = 0;
+
+    if (lim != -1)
+    {
+        printf("%s %ld\n", msg, lim);
+    }
+    else
+    {
+        if (errno == 1)
+        {
+            printf("%s (indeterminate)\n", msg);
+        }
+        else
+        {
+            errExit("sysconfg %s", msg);
+        }
+    }
+
+    int main(int argc, char* argv[])
+    {
+        sysconfPrint("_SC_ARG_MAX:   ", _SC_ARG_MAX);
+        sysconfPrint("_SC_LOGIN_NAME_MAX:", _SC_LOGIN_NAME_MAX);
+        sysconfPrint("_SC_OPEN_MAX: ", _SC_OPEN_MAX);
+        sysconfPrint("_SC_NGROUPS_MAX: ", _SC_NGROUPS_MAX);
+        sysconfPrint("_SC_PAGESIZE:", _SC_PAGESIZE);
+        sysconfPrint("_SC_RTSIG_MAX: ", _SC_RTSIG_MAX);
+
+        exit(EXIT_SUCCESS);
+    }
+}
