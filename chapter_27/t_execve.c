c47a596830a8d96211fe9db7dd0f61a9378010f5
Wei jianxing <jx.wei@outlook.com>
2021-06-04 21:09:24 +0800

    Update code

---
+++ chapter_27/t_execve.c
@@ -0,0 +1,29 @@
+#include "tlpi_hdr.h"
+
+int main(int argc, char* argv[])
+{
+    char* argVec[10];
+    char * envVec[] = {"GREET=salut", "BYE=adieu", NULL};
+
+    if(argc != 2|| strcmp(argv[1], "--help") == 0)
+    {
+        usageErr("%s pathname\n", argv[0]);
+    }
+
+    argVec[0] = strrchr(argv[1], '/');
+    if (argVec[0] != NULL)
+    {
+        argVec[0]++;
+    }
+    else
+    {
+        argVec[0] = argVec[1];
+    }
+
+    argVec[1] = "hello world";
+    argVec[2] = "goodbye";
+    argVec[3] = NULL;
+
+    execve(argv[1]);
+    errExit("execve");
+}
