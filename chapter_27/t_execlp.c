4b16d4a760eb17891e2216d0d3ae5cdf6351d5ff
Wei jianxing <jx.wei@outlook.com>
2021-06-06 20:12:40 +0800

    Update code

---
+++ chapter_27/t_execlp.c
@@ -0,0 +1,13 @@
+#include "tlpi_hdr.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2 || strcmp(argv[1], "--help") == 0)
+    {
+        usageErr("%s pathname\n", argv[0]);
+
+        execlp(argv[1], argv[1], "hello world", (char* )NULL);
+        errExit("execlp");
+        
+    }
+}
