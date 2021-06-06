7d977a0390b9440ea3cf907174b0e1a7e5d860fd
Wei jianxing <jx.wei@outlook.com>
2021-06-06 20:35:40 +0800

    Update code

---
+++ chapter_27/t_execl.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "tlpi_hdr.h"
+
+int main(argc, char* argv[])
+{
+    printf("Initial value of USER: %s\n", getenv("USER"));
+    if (putenv("USER=britta") != 0)
+    {
+        errExit("putenv");
+    }
+
+    execl("/usr/bin/printenv", "printenv", "USER", "SHELL", (char*) NULL);
+    errExit("execl");
+}
