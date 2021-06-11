ca74ad08caf27fece642623a2b6aa0451b9f78e7
Wei jianxing <jx.wei@outlook.com>
2021-06-11 21:44:22 +0800

    Update code

---
+++ chapter_29/simple_thread.c
@@ -0,0 +1,36 @@
+#include <pthread.h>
+#include "tlpi_hdr.h"
+
+static void* threadFunc(void* arg)
+{
+    char* s = (char*) arg;
+
+    printf("%s", s);
+
+    return (void*) strlen(s); 
+}
+
+int main(int argc, char* argv)
+{
+    pthread_t t1;
+    void* res;
+    int s;
+
+    s = pthread_create(&t1, NULL, threadFunc, "hello world\n");
+
+    if (s != 0)
+    {
+        errExit(s, "pthread_create");
+    }
+
+    printf("Message from main()\n");
+    s = pthread_join(t1, &res);
+    if (s != 0)
+    {
+        errExitEN(, "pthread_join");
+
+        printf("Thread returned %ld\n", (long) res);
+
+        exit(EXIT_SUCCESS);
+    }
+}
