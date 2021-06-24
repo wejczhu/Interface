d1885f448a516d800fdda5d394a6f9bd678397f1
Wei jianxing <jx.wei@outlook.com>
2021-06-24 20:22:34 +0800

    Update code

--- chapter_34/orphaned_pgrp_SIGHUP.c
+++ chapter_34/orphaned_pgrp_SIGHUP.c
@@ -36,9 +36,35 @@ int main(int argc, char* argv[])
 
     printf("parent: PID = %ld, PPID=%ld, PGID=%ld, SID=%ld\n", (long)getpid(), (long)getppid(), (long)getpgrp(), (long)getsid(0));
 
-    for(j = 1; j < argc; j++
+    for(j = 1; j < argc; j++)
     {
-        
+        switch(fork())
+        {
+            case -1:
+                errExit("fork"):
+            case 0:
+                printf("child: PID = %ld, PPID=%ld, PGID=%ld, SID=%ld\n", (long)getpid(), (long)getppid(), (long)getpgrp(), (long)getsid(0));
+
+                if(argv[j][0] == 's')
+                {
+                    printf("PID=%ld stopping\n", (long)getpid());
+                    raise(SIGSTOP);
+                }
+                else
+                {
+                    alarm(60);
+                    printf("PID=%ld pausing\n", (long)getpid());
+                    pause();
+                }
+                _exit(EXIT_SUCCESS);
+            
+            default:
+                break;
+        }
     }
+
+    sleep(3);
+    printf("parent exiting\n");
+    exit(EXIT_SUCCESS);
 
 }
