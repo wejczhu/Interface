be41015a7fbd80952ca42de15c296096f8ba5301
Wei jianxing <jx.wei@outlook.com>
2021-08-17 20:39:50 +0800

    Update code

---
+++ chapter_55/create_pid_file.c
@@ -0,0 +1,59 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "region_locking.h"
+#include "tlpi_hdr.h"
+#include "create_pid_file.h"
+
+#define BUF_SIZE 100
+
+int createPidFile(const char* progName, const char* pidFile, int flags)
+{
+    int fd;char buf[BUF_SIZE];
+    fd = open(pidFile, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    if(fd == -1)
+    {
+        errExit("Could not open PID file %s", pidFile);
+    }
+
+    if(flags & CPF_CLOEXEC)
+    {
+        flags = fcntl(fd, F_GETLK);
+
+        if(flags == -1)
+        {
+            errExit("Could not get flags for PID file %s", pidFile);
+        }
+
+        flags |= FD_CLOEXEC;
+        if(fcntl(fd, F_SETFD, flags) == -1)
+        {
+            errExit("Could not set flags for PID file %s", pidFile);
+        }
+
+    }
+
+    if(lockRegion(fd, F_WRLCK, SEEK_SET, 0, 0) == -1)
+    {
+        if(errno == EAGAIN || errno == EACCES)
+        {
+            fatal("PID file '%s' is locked; probably '%s' is already running", pidFile, progName);
+        }
+        else
+        {
+            errExit("Unable to lock PID file '%s'", pidFile);
+        }
+    }
+    if(ftruncate(fd, 0) == -1)
+    {
+        errExit("could not truncate PId file '%s'", pidFile);
+    }
+
+    snprintf(buf, BUF_SIZE, "%ld", (long)getpid());
+    if(write(fd, buf, strlen(buf) != strlen(buf)))
+    {
+        fatal("Writing to PId file '%s'", pidFile);
+    }
+
+    return fd;
+}
+
