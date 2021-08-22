267c97f7b64e9523de97c3bdef618a22868f876e
Wei jianxing <jx.wei@outlook.com>
2021-08-22 21:51:01 +0800

    Update code

---
+++ chapter_59/read_line.c
@@ -0,0 +1,62 @@
+#include <unistd.h>
+#include <errno.h>
+#include "tlpi_hdr.h"
+
+ssize_t readLine(int fd, void* buffer, size_t n)
+{
+    ssize_t numRead;
+    size_t totRead;
+    char* buf;
+    char ch;
+
+    if(n <= 0 || buffer == NULL)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+
+    buf = buffer;
+
+    totRead = 0;
+    for(;;)
+    {
+        numRead = read(fd, &ch, 1);
+        if(numRead == -1)
+        {
+            if(errno == EINTR)
+            {
+                continue;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        else if(numRead == 0)
+        {
+            if(totRead == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            if(totRead < n - 1)
+            {
+                totRead++;
+                *buf++ = ch;
+            }
+            if(ch == "\n")
+            {
+                break;
+            }
+        }
+
+        *buf = '\0';
+        return totRead;
+    }
+}
