ab0a53244a5ce0b5eb7dee055510b8436953a47a
Wei jianxing <jx.wei@outlook.com>
2021-08-28 21:16:17 +0800

    Update code

---
+++ chapter_59/t_gethostbyname.c
@@ -0,0 +1,48 @@
+#define _BSD_SOURCE
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "tlpi_hdr.h"
+
+int main(int argc, char* argv[])
+{
+    struct hostent* h;
+    char** pp;
+    char str[INET6_ADDRSTRLEN];
+
+    for(argv++; *argv != NULL; argv++)
+    {
+        h = gethostbyname(*argv);
+        if(h == NULL)
+        {
+            fprintf(strerr, "gethostbyname() filed for '%s' : %s\n", *argv, hstrerror(h_errno));
+
+            continue;
+        }
+
+        printf("canonical name: %s\n", h->h_name);
+
+        printf(" alias(es): ");
+
+        for(pp = h->h_aliases; *pp != NULL; pp++)
+        {
+            printf("%s", *pp);
+        }
+        printf("\n");
+        printf(" address type: %s\n", (h->h_addrtype == AF_INET) ? "AF_INET" : (h->h_addrtype == AF_INET6) ? "AF_INET6" : "???");
+
+        if(h->h_addrtype == AF_INET || h->h_addrtype == AF_INET6)
+        {
+            printf("address(es): ");
+            for(pp = h->h_addr_list; *pp != NULL; pp++)
+            {
+                printf("%s", inet_ntop(h-<h_addrtype, *pp, str, INET6_ADDRSTRLEN));
+            }
+
+            printf("\n");
+        }
+    }
+
+    exit(EXIT_SUCCESS);
+}
