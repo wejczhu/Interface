f6c14a512ef6287837bf094a0303d817a5168f52
Wei jianxing <jx.wei@outlook.com>
2021-09-07 20:48:58 +0800

    Update code

--- chapter_62/test_tty_function.c
+++ chapter_62/test_tty_function.c
@@ -78,4 +78,109 @@ int main(int argc, char* argv[])
 
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
+
+    if(argc > 1)
+    {
+        if(ttySetCbreak(STDIN_FILENO, &userTermios) == -1)
+        {
+            errExit("ttySetCbreak");
+        }
+
+        sa.sa_handler = handler;
+
+        if(sigaction(SIGQUIT, NULL, &prev) == -1)
+        {
+            errExit("sigaction");
+        }
+        if(prev.sa_halder != SIG_IGN)
+        {
+            if(sigaction(SIGQUIT, &sa, NULL) == -1)
+            {
+                errExit("sigaction");
+            }
+        }
+
+        if(sigaction(SIGINT, NULL, &prev) == -1)
+        {
+            errExit("sigaction");
+        }
+        if(prev.sa_handler != SIG_IGN)
+        {
+            if(sigaction(SIGINT, &sa, NULL) == -1)
+            {
+                errExit("sigaction");
+            }
+        }
+
+        sa.sa_handler = tstpHandler;
+        if(sigaction(SIGTSTP, NULL, &prev) == -1)
+        {
+            errExit("sigaction");
+        }
+
+        if(prev.sa_handler != SIG_IGN)
+        {
+            if(sigaction(SIGTSTP, &sa, NULL) == -1)
+            {
+                errExit("sigaction");
+            }
+        }
+    }
+    else
+    {
+        if(ttySetRaw(STDIN_FILENO, &userTermios) == -1)
+        {
+            errExit("ttySetRaw");
+        }
+    }
+
+    sa.sa_handler = handler;
+    if(sigaction(SIGTERM, &sa, NULL) == -1)
+    {
+        errExit("sigaction");
+    }
+
+    setbuf(stdout, NULL);
+    for(;;)
+    {
+        n = read(STDIN_FILENO, &ch, 1);
+        if(n == -1)
+        {
+            errMsg("read");
+            break;
+        }
+
+        if(n == 0)
+        {
+            break;
+        }
+
+        if(isalpha(unsigned char) ch))
+        {
+            putchar(tolower((unsigned char) ch));
+        }
+        else if(ch == '\n' || ch == 'r')
+        {
+            putchar(ch);
+        }
+        else if(iscntrl((unsigned char) ch))
+        {
+            printf("^%c", ch ^ 64);
+        }
+        else
+        {
+            putchar('*');
+        }
+
+        if(ch == 'q')
+        {
+            break;
+        }
+    }
+
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &userTermios) == -1)
+    {
+        errExit("tcsetattr");
+    }
+    exit(EXIT_SUCCESS0;)
 }
